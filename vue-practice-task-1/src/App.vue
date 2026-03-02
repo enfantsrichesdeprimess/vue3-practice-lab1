c3153743bc66bc7c877662a6ea2cba117e640fcb
enfantrich <alexeytretyter@gmail.com>
2026-03-03 01:40:01 +0700

    Создание экземпляров Vue

---
+++ vue-practice-task-1/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>{{ product }}</h1>
+  <p>{{ description }}</p>
+</template>
+
+<!--разместил тут и далее этот код пользуясь станлартом vue3(Options api)-->
+<script>
+export default {
+  data() {
+    return {
+      product: 'Socks',
+      description: 'A pair of warm, fuzzy socks'
+    };
+  },
+};
+</script>
---
+++ vue-practice-task-1/src/main.js
@@ -0,0 +1,8 @@
+// index app
+import { createApp } from 'vue';
+import App from './App.vue';
+
+createApp(App).mount('#app');
+
+
+
