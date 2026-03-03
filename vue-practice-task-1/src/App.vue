79acd3ed3fd571403939b02c90a9434c9b544f43
enfantrich <alexeytretyter@gmail.com>
2026-03-03 17:54:15 +0700

    Привязка аттрибутов+оптимизация прошлой версии

--- vue-practice-task-1/src/App.vue
+++ vue-practice-task-1/src/App.vue
@@ -1,6 +1,12 @@
 <template>
-  <h1>{{ product }}</h1>
-  <p>{{ description }}</p>
+  <div class="product-image">
+    <img :alt="altText" :src="image" />
+  </div>
+  <div class="product-info">
+    <h1>{{ product }}</h1>
+    <p>{{ description }}</p>
+    <a :href="link"> More product like this </a>
+  </div>
 </template>
 
 <!--разместил тут и далее этот код пользуясь станлартом vue3(Options api)-->
@@ -9,7 +15,10 @@ export default {
   data() {
     return {
       product: 'Socks',
-      description: 'A pair of warm, fuzzy socks'
+      description: 'A pair of warm, fuzzy socks',
+      image: 'assets/socks.jpg',
+      altText: 'Pair Of Greeeeeeeen Socks',
+      link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
     };
   },
 };
