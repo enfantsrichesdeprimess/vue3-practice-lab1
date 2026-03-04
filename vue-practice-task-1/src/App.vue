b335c894fb71fd870a93305336384c9876e4c526
enfantrich <alexeytretyter@gmail.com>
2026-03-04 13:51:19 +0700

    вычисляемые свойства

--- vue-practice-task-1/src/App.vue
+++ vue-practice-task-1/src/App.vue
@@ -4,28 +4,28 @@
       <img :alt="altText" :src="image" />
     </div>
       <div class="product-info">
-        <h1>{{ product }}</h1>
+        <h1>{{ title }}</h1>
         <p>{{ description }}</p>
         <ul>
           <li v-for="detail in details">{{ detail }}</li>
         </ul>
         <div
             class="color-box"
-            v-for="variant in variants"
+            v-for="(variant, index) in variants"
             :key="variant.variantId"
             :style="{ backgroundColor:variant.variantColor }"
-            @mouseover="updateProduct(variant.variantImage)"
+            @mouseover="updateProduct(index)"
         >
         </div>
         <ul>
           <li v-for="size in sizes">{{ size }}</li>
         </ul>
         <div class="sale-text">
-          <span v-if="inventory <= 100 && sale === true && inStock === true">Big Sale!!!</span>
+          <span>{{ onSale }}</span>
           </div>
         <div>
-          <p v-if="inventory > 10">In stock</p>
-          <p v-else-if="inventory <= 10 && inventory > 0">Almost sold out!</p>
+          <p v-if="inStock > 0">In stock</p>
+          <p v-else-if="inStock <= 10 && inStock > 0">Almost sold out!</p>
           <p v-else :class="{ outOfStock: !inStock }">Out of stock</p>
         </div>
         <a :href="link"> More product like this </a>
@@ -47,13 +47,13 @@
 export default {
   data() {
     return {
+      brand: 'Vue Mastery',
       product: 'Socks',
       description: 'A pair of warm, fuzzy socks',
-      image: 'assets/socks.jpg',
-      altText: 'Pair Of Greeeeeeeen Socks',
+      selectedVariant: 0,
+      altText: 'Pair Of Socks',
       link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
-      inStock: false,
-      inventory: 0,
+      inventory: 100,
       sale: true,
       details: ['80% cotton', '20% polyester', 'Gender-neutral'],
       variants: [
@@ -61,11 +61,13 @@ export default {
           variantId: 2234,
           variantColor: 'green',
           variantImage: "assets/socks.jpg",
+          variantQuantity: 100
         },
         {
           variantId: 2235,
           variantColor: 'blue',
           variantImage: "assets/bluesocks.jpg",
+          variantQuantity: 0
         }
       ],
       sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
@@ -76,11 +78,30 @@ export default {
     addToCart () {
       this.cart += 1;
     },
-    updateProduct (variantImage) {
-      this.image = variantImage;
+    updateProduct (index) {
+      this.selectedVariant = index;
+      console.log(index);
     },
     deleteFromCart () {
       this.cart -= 1;
+    }
+  },
+  computed: {
+    title() {
+      return this.brand + ' ' + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].variantImage;
+    },
+    inStock(){
+      return this.variants[this.selectedVariant].variantQuantity
+    },
+    onSale() {
+      if (this.sale===true && this.variants[this.selectedVariant].variantQuantity <= 100 && this.variants[this.selectedVariant].variantQuantity >=1  ) {
+        return this.brand + ' ' + this.product + ' IS ON BIG SALEE!!!'
+      }
+      else {
+      }
     }
   }
 };
