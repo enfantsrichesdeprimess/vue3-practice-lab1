f5b5dd038ad0f5b3c6313ecec8fedc3592d963ac
enfantrich <alexeytretyter@gmail.com>
2026-03-04 13:09:30 +0700

    обработка событий + привязка классов и стилей

--- vue-practice-task-1/src/App.vue
+++ vue-practice-task-1/src/App.vue
@@ -9,19 +9,35 @@
         <ul>
           <li v-for="detail in details">{{ detail }}</li>
         </ul>
-        <div v-for="variant in variants" :key="variant.variantId">
-          <p>{{ variant.variantColor }}</p>
+        <div
+            class="color-box"
+            v-for="variant in variants"
+            :key="variant.variantId"
+            :style="{ backgroundColor:variant.variantColor }"
+            @mouseover="updateProduct(variant.variantImage)"
+        >
         </div>
         <ul>
           <li v-for="size in sizes">{{ size }}</li>
         </ul>
         <div class="sale-text">
-          <span v-if="inventory <= 100 && sale === true">Big Sale!!!</span>
+          <span v-if="inventory <= 100 && sale === true && inStock === true">Big Sale!!!</span>
           </div>
-        <p v-if="inventory > 10">In stock</p>
-        <p v-else-if="inventory <= 10 && inventory > 0">Almost sold out!</p>
-        <p v-else>Out of stock</p>
+        <div>
+          <p v-if="inventory > 10">In stock</p>
+          <p v-else-if="inventory <= 10 && inventory > 0">Almost sold out!</p>
+          <p v-else :class="{ outOfStock: !inStock }">Out of stock</p>
+        </div>
         <a :href="link"> More product like this </a>
+        <div class="cart">
+          <p>Cart({{ cart }})</p>
+        </div>
+        <br><button
+          v-on:click="addToCart"
+          :disabled="!inStock"
+          :class="{ disabledButton: !inStock }"
+      >Add to cart</button>
+        <br><button @click="deleteFromCart">Delete one from cart</button>
       </div>
   </div>
 </template>
@@ -36,22 +52,36 @@ export default {
       image: 'assets/socks.jpg',
       altText: 'Pair Of Greeeeeeeen Socks',
       link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
-      inStock: true,
-      inventory: 100,
+      inStock: false,
+      inventory: 0,
       sale: true,
       details: ['80% cotton', '20% polyester', 'Gender-neutral'],
       variants: [
         {
           variantId: 2234,
-          variantColor: 'green'
+          variantColor: 'green',
+          variantImage: "assets/socks.jpg",
         },
         {
           variantId: 2235,
-          variantColor: 'blue'
+          variantColor: 'blue',
+          variantImage: "assets/bluesocks.jpg",
         }
       ],
       sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+      cart: 0,
     };
   },
+  methods: {
+    addToCart () {
+      this.cart += 1;
+    },
+    updateProduct (variantImage) {
+      this.image = variantImage;
+    },
+    deleteFromCart () {
+      this.cart -= 1;
+    }
+  }
 };
 </script>
